82f41886778d870cb235166d36eb5fd3b18f32d0
prajubs7 <prajaktashinde5810@gmail.com>
2026-09-01 15:04:12 +0530

    TEST: CONFIGURE REACT NATIVE JEST ENVIORNMENT

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  moduleNameMapper: {
+    '^react-native-gesture-handler$': '<rootDir>/__mocks__/react-native-gesture-handler.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|react-native-gesture-handler|react-redux|redux-persist|react-native-safe-area-context|react-native-screens)/)',
+  ],
 };
